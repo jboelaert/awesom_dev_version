c58e30d811dab89297a6db96e8fb28aa6d79c6b6
Jan Sodoge <janso162@student.liu.se>
2020-11-06 12:28:16 +0100

    Update server.R

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -89,7 +89,7 @@ shinyServer(function(input, output, session) {
         input_dataFile_datapath = input$dataFile$datapath)
     }
     
-    isolate({values$codetxt$dataread <- imported_file_object[[2]]})
+    values$codetxt$dataread <- imported_file_object[[2]]
     imported_file_object[[1]]
   })
   
@@ -119,30 +119,17 @@ shinyServer(function(input, output, session) {
   
   
   
-  ## Download interactive plot (download widget)
-  output$downloadInteractive <- downloadHandler(
-    filename= paste0(Sys.Date(), "-aweSOM.html"), 
-    content= function(file) {
-      if (is.null(ok.som())) return(NULL)
-      widg <- aweSOM::aweSOMplot(ok.som= ok.som(), 
-                                 ok.sc= ok.sc(), 
-                                 ok.data= ok.data(), 
-                                 ok.trainrows= ok.trainrows(), 
-                                 graphType= input$graphType, 
-                                 plotNames= input$plotNames, 
-                                 plotVarMult= input$plotVarMult, 
-                                 plotVarOne= input$plotVarOne, 
-                                 plotOutliers= input$plotOutliers,
-                                 plotEqualSize= input$plotEqualSize,
-                                 contrast= input$contrast, 
-                                 average_format= input$average_format,
-                                 plotSize= input$plotSize, 
-                                 palsc= input$palsc, 
-                                 palplot= input$palplot, 
-                                 plotRevPal= input$plotRevPal)
-      htmlwidgets::saveWidget(widg, file = file)
-    }) 
-
+  ### check placement in server.R for this element
+  output$downloadData_interactive <- downloadHandler(
+       filename = function() {
+         paste0("interactive_download", gsub(pattern = " ", replacement = "_", Sys.time()),  ".html")
+       },
+       content = 
+         function(file) {
+           file.copy("export/awesom_exported_version.html", file)
+       }
+     )
+  
   
   
   
@@ -220,7 +207,6 @@ shinyServer(function(input, output, session) {
 
   ok.traindat <- reactive({
     if (input$trainbutton == 0) return(NULL)
-    input$retrainButton
     isolate({
       #to make externalized functions more useable this part is placed outside of the (Jan)
       varSelected <- as.logical(sapply(paste0("trainVarChoice", colnames(ok.data())), 
@@ -250,20 +236,19 @@ shinyServer(function(input, output, session) {
     if (is.null(dat$dat)) {
       return(NULL)
     }
-    isolate({
-      res <- aweSOM:::ok.som.function(ok.traindat = dat, 
-                                      input_trainSeed = input$trainSeed, 
-                                      input_kohInit = input$kohInit,
-                                      input_kohDimy = input$kohDimy, 
-                                      input_kohDimx = input$kohDimx, 
-                                      input_kohTopo = input$kohTopo, 
-                                      input_trainRlen = input$trainRlen,
-                                      input_trainAlpha1 = input$trainAlpha1, 
-                                      input_trainAlpha2 = input$trainAlpha2, 
-                                      input_trainRadius1 = input$trainRadius1, 
-                                      input_trainRadius2 = input$trainRadius2)
-      values$codetxt$train <- res$codeTxt
-    })
+    
+    res <- aweSOM:::ok.som.function(ok.traindat = dat, 
+                           input_trainSeed = input$trainSeed, 
+                           input_kohInit = input$kohInit,
+                           input_kohDimy = input$kohDimy, 
+                           input_kohDimx = input$kohDimx, 
+                           input_kohTopo = input$kohTopo, 
+                           input_trainRlen = input$trainRlen,
+                           input_trainAlpha1 = input$trainAlpha1, 
+                           input_trainAlpha2 = input$trainAlpha2, 
+                           input_trainRadius1 = input$trainRadius1, 
+                           input_trainRadius2 = input$trainRadius2)
+    values$codetxt$train <- res$codeTxt
     
     ## After training, set new seed value in training panel
     updateNumericInput(session, "trainSeed", value= sample(1e5, 1))
@@ -280,7 +265,7 @@ shinyServer(function(input, output, session) {
   ## Compute superclasses when ok.som or superclass options changes
   ok.hclust <- reactive({
     if(!is.null(ok.som())){
-      hclust(dist(ok.som()$codes[[1]]), input$sup_clust_hcmethod)
+      hclust(dist(ok.som()$codes[[1]]), "ward.D2")
     }
   })
   
@@ -353,9 +338,16 @@ shinyServer(function(input, output, session) {
     isolate(cat(paste0("Training options: rlen = ", input$trainRlen, 
                        " ; alpha = (", input$trainAlpha1, ", ", input$trainAlpha2, ") ; ",
                        "radius = (", input$trainRadius1, ", ", input$trainRadius2, "), ", 
-                       "random seed = ", ok.som()$seed, ".\n")))
-
-    ok.qual()
+                       "random seed = ", ok.som()$seed, ".")))
+    
+    cat("\n\n## Quality measures:\n")
+    cat("* Quantization error     : ", ok.qual()$err.quant, "\n")
+    cat("* (% explained variance) : ", ok.qual()$err.varratio, "\n")
+    cat("* Topographic error      : ", ok.qual()$err.topo, "\n")
+    cat("* Kaski-Lagus error      : ", ok.qual()$err.kaski, "\n")
+    cat("\n## Number of obs. per map cell:")
+    table(factor(ok.som()$unit.classif, 
+                 levels= 1:nrow(ok.som()$grid$pts)))
   })
   
  
@@ -387,7 +379,6 @@ shinyServer(function(input, output, session) {
     })
   })
   
-
   ## Update max nb superclasses
  
   
@@ -446,7 +437,6 @@ shinyServer(function(input, output, session) {
   #Code function important for dendogram and further plots
   ## Dendrogram
   output$plotDendrogram <- renderPlot({
-    if (input$sup_clust_method != "hierarchical") return(NULL)
     values$codetxt$plot <- paste0("\n## Plot superclasses dendrogram\n", 
                                   "aweSOM::aweSOMdendrogram(ok.som, superclust, ", 
                                   input$kohSuperclass, ")\n")
@@ -524,118 +514,85 @@ shinyServer(function(input, output, session) {
   
   
   
-  # ## Fancy JS Plots
-  # output$thePlot <- reactive({
-  #   if (is.null(ok.som()) | !(input$graphType %in% c("Radar", 
-  #                                                    "Camembert", "CatBarplot",
-  #                                                    "Barplot", "Boxplot", 
-  #                                                    "Color", "Star", 
-  #                                                    "Hitmap", "Line", 
-  #                                                    "Names", "UMatrix")))
-  #     return(NULL) # si on n'a pas calculé, on donne NULL à JS
-  # 
-  #   plot.data <- isolate(ok.data()[ok.trainrows(), ])
-  #   if(is.null(plot.data)) return(NULL)
-  #   # Obs names per cell for message box
-  #   if (is.null(input$plotNames)){
-  #     return(NULL)
-  #     
-  #   } 
-  #   if (input$plotNames == "(rownames)") {
-  #     plotNames.var <- rownames(plot.data)
-  #   } 
-  #   else {
-  #     plotNames.var <- as.character(plot.data[, input$plotNames])
-  #   }
-  #   
-  #   cellNames <- unname(lapply(split(plotNames.var, ok.clust()), 
-  #                       function(x) paste(sort(x), collapse= ", "))) # "&#13;&#10;" "<br />"
-  #   
-  #   if (input$graphType %in% c("Radar", "Star", "Barplot", "Boxplot", "Line")) {
-  #     if (is.null(input$plotVarMult)) return(NULL)
-  #     plotVar <- input$plotVarMult
-  #     data <- plot.data[, plotVar]
-  #   } else if (input$graphType %in% c("Color", "Camembert", "CatBarplot")) {
-  #     if (is.null(input$plotVarOne)) return(NULL)
-  #     plotVar <- input$plotVarOne
-  #     data <- plot.data[, plotVar]
-  #   } else if (input$graphType %in% c("Hitmap")) {
-  #     plotVar <- NULL
-  #     data <- NULL
-  #   } else if (input$graphType %in% c("Names")) {
-  #     plotVar <- NULL
-  #     data <- as.character(plot.data[, input$plotVarOne])
-  #   } else if (input$graphType == "UMatrix") {
-  #     plotVar <- NULL
-  #     proto.gridspace.dist <- as.matrix(dist(ok.som()$grid$pts))
-  #     proto.dataspace.dist <- as.matrix(dist(ok.som()$codes[[1]]))
-  #     proto.dataspace.dist[round(proto.gridspace.dist, 3) > 1] <- NA
-  #     proto.dataspace.dist[proto.gridspace.dist == 0] <- NA
-  #     data <- rowMeans(proto.dataspace.dist, na.rm= T)[ok.clust()]
-  #     plotVar <- "Mean distance to neighbours"
-  #   }
-  #   
-  #   options <- list(equalSize= input$plotEqualSize)
-  #   
-  # 
-  #   if(input$contrast == "contrast")  contrast <- "contrast"
-  #   else if(input$contrast == "range") contrast <- "range"
-  #   else if(input$contrast == "no_contrast") contrast <- "no_contrast" 
-  #   
-  #   the.average_format <- switch(input$average_format, "mean"= "mean", 
-  #                                "median"= "median", "prototypes"= "prototypes")
-  #   
-  #   graphType <- ifelse(input$graphType == "UMatrix", "Color", input$graphType)
-  #   
-  #   values$codetxt$plot <- paste0("\n## Plot interactive ** graph \n", 
-  #                                 "# aweSOM::aweSOMplot(the, arguments)")
-  # 
-  #   aweSOM:::getPlotParams(graphType, ok.som(), ok.sc(),  
-  #                          data, input$plotSize, plotVar, contrast,
-  #                          input$palsc, input$palplot, cellNames,
-  #                          input$plotOutliers, input$plotRevPal, options, 
-  #                          the.average_format)
-  # })
-  
-  
-  ## Fancy JS plots through widget
-  output$theWidget <- aweSOM:::renderaweSOM({
-    values$codetxt$plot <- paste0(
-      "\n## Interactive plot\n", 
-      "aweSOM::aweSOMplot(ok.som = ok.som, ok.sc = superclasses, ok.data = ok.data,\n", 
-      "                   ok.trainrows = c(", paste(ok.trainrows(), collapse= ", "), "),\n", 
-      "                   graphType = '", input$graphType, 
-      "', plotNames = '", input$plotNames, "',\n",
-      "                   plotVarMult = c('", paste(input$plotVarMult, collapse= "', '"), 
-      "'), plotVarOne = '", input$plotVarOne, "',\n",
-      "                   plotEqualSize = ", input$plotEqualSize, 
-      ", contrast = '", input$contrast, "',\n",
-      "                   average_format = '", input$average_format,
-      "', plotSize = ", input$plotSize, ",\n",
-      "                   palsc = '", input$palsc, 
-      "', palplot = '", input$palplot, "', plotRevPal = ", input$plotRevPal, ")\n")
+  ## Fancy JS Plots
+  output$thePlot <- reactive({
+    if (is.null(ok.som()) | !(input$graphType %in% c("Radar", 
+                                                     "Camembert", "CatBarplot",
+                                                     "Barplot", "Boxplot", 
+                                                     "Color", "Star", 
+                                                     "Hitmap", "Line", 
+                                                     "Names", "UMatrix")))
+      return(NULL) # si on n'a pas calculé, on donne NULL à JS
+
+    plot.data <- isolate(ok.data()[ok.trainrows(), ])
+    if(is.null(plot.data)) return(NULL)
+    # Obs names per cell for message box
+    if (is.null(input$plotNames)){
+      return(NULL)
+      
+    } 
+    if (input$plotNames == "(rownames)") {
+      plotNames.var <- rownames(plot.data)
+    } 
+    else {
+      plotNames.var <- as.character(plot.data[, input$plotNames])
+    }
     
-    aweSOM:::aweSOMwidget(ok.som= ok.som(), 
-                          ok.sc= ok.sc(), 
-                          ok.data= ok.data(), 
-                          ok.trainrows= ok.trainrows(), 
-                          graphType= input$graphType, 
-                          plotNames= input$plotNames, 
-                          plotVarMult= input$plotVarMult, 
-                          plotVarOne= input$plotVarOne, 
-                          plotOutliers= input$plotOutliers,
-                          plotEqualSize= input$plotEqualSize,
-                          contrast= input$contrast, 
-                          average_format= input$average_format,
-                          plotSize= input$plotSize, 
-                          palsc= input$palsc, 
-                          palplot= input$palplot, 
-                          plotRevPal= input$plotRevPal)
+    cellNames <- unname(lapply(split(plotNames.var, ok.clust()), 
+                        function(x) paste(sort(x), collapse= ", "))) # "&#13;&#10;" "<br />"
+    
+    if (input$graphType %in% c("Radar", "Star", "Barplot", "Boxplot", "Line")) {
+      if (is.null(input$plotVarMult)) return(NULL)
+      plotVar <- input$plotVarMult
+      data <- plot.data[, plotVar]
+    } else if (input$graphType %in% c("Color", "Camembert", "CatBarplot")) {
+      if (is.null(input$plotVarOne)) return(NULL)
+      plotVar <- input$plotVarOne
+      data <- plot.data[, plotVar]
+    } else if (input$graphType %in% c("Hitmap")) {
+      plotVar <- NULL
+      data <- NULL
+    } else if (input$graphType %in% c("Names")) {
+      plotVar <- NULL
+      data <- as.character(plot.data[, input$plotVarOne])
+    } else if (input$graphType == "UMatrix") {
+      plotVar <- NULL
+      proto.gridspace.dist <- as.matrix(dist(ok.som()$grid$pts))
+      proto.dataspace.dist <- as.matrix(dist(ok.som()$codes[[1]]))
+      proto.dataspace.dist[round(proto.gridspace.dist, 3) > 1] <- NA
+      proto.dataspace.dist[proto.gridspace.dist == 0] <- NA
+      data <- rowMeans(proto.dataspace.dist, na.rm= T)[ok.clust()]
+      plotVar <- "Mean distance to neighbours"
+    }
+    
+    options <- list(equalSize= input$plotEqualSize)
+    
+
+    if(input$contrast == "contrast")  contrast <- "contrast"
+    else if(input$contrast == "range") contrast <- "range"
+    else if(input$contrast == "no_contrast") contrast <- "no_contrast" 
+    
+    the.average_format <- switch(input$average_format, "mean"= "mean", 
+                                 "median"= "median", "prototypes"= "prototypes")
+    
+    graphType <- ifelse(input$graphType == "UMatrix", "Color", input$graphType)
+    
+    values$codetxt$plot <- paste0("\n## Plot interactive ** graph \n", 
+                                  "# aweSOM::aweSOMplot(the, arguments)")
+
+    aweSOM:::getPlotParams(graphType, ok.som(), ok.sc(),  
+                           data, input$plotSize, plotVar, contrast,
+                           input$palsc, input$palplot, cellNames,
+                           input$plotOutliers, input$plotRevPal, options, 
+                           the.average_format)
   })
+  
+
+  
   
   
   ## warning for smooth distance hex based plot
-  output$smooth_dist_warning <- renderPrint({
+  output$smooth_dist_warning <- renderText({
     if(input$kohTopo == "hexagonal"){ 
       
       print("This might be a biased version since the topology of a hexagonal grid cannnot be account
@@ -752,37 +709,15 @@ shinyServer(function(input, output, session) {
            values$codetxt$sc, 
            if (!is.null(ok.som())) paste0(
              "\n## Quality measures:\n",
-             "aweSOM::somQuality(ok.som, dat)\n",
+             "ok.qual <- aweSOM::somQuality(ok.som, dat)\n",
+             'cat("* Quantization error     : ", ok.qual$err.quant, "\\n",\n',
+             '    "* (% explained variance) : ", ok.qual$err.varratio, "\\n",\n',
+             '    "* Topographic error      : ", ok.qual$err.topo, "\\n",\n',
+             '    "* Kaski-Lagus error      : ", ok.qual$err.kaski, "\\n")\n',
+             '## Number of obs. per map cell:\n',
+             'table(factor(ok.som$unit.classif, levels= 1:nrow(ok.som$grid$pts)))\n',
              values$codetxt$plot))
   })
-  
-  ## Trying to make the code more pretty with RMarkdown, failed (works, but not more pretty and messes with the css)
-  # output$codeTxt2 <- shiny::renderUI({
-  #   tmp <- paste0("\n## Import Data\n", 
-  #          values$codetxt$dataread, 
-  #          "\n## Build training data\n",
-  #          values$codetxt_traindat$traindat, 
-  #          "\n## Train SOM\n", 
-  #          values$codetxt$train, "\n",
-  #          values$codetxt$sc, 
-  #          if (!is.null(ok.som())) paste0(
-  #            "\n## Quality measures:\n",
-  #            "ok.qual <- aweSOM::somQuality(ok.som, dat)\n",
-  #            'cat("* Quantization error     : ", ok.qual$err.quant, "\\n",\n',
-  #            '    "* (% explained variance) : ", ok.qual$err.varratio, "\\n",\n',
-  #            '    "* Topographic error      : ", ok.qual$err.topo, "\\n",\n',
-  #            '    "* Kaski-Lagus error      : ", ok.qual$err.kaski, "\\n")\n',
-  #            '## Number of obs. per map cell:\n',
-  #            'table(factor(ok.som$unit.classif, levels= 1:nrow(ok.som$grid$pts)))\n',
-  #            values$codetxt$plot))
-  #   HTML(markdown::markdownToHTML(text= knitr::knit(text= paste0("---\n", 
-  #                                                           "title: 'aweSOMcode'\n", 
-  #                                                           "output: hmtl_document\n", 
-  #                                                           "---\n", 
-  #                                                           "```{r eval=FALSE}\n", 
-  #                                                           tmp, "\n```\n"), 
-  #                                              quiet= T)))
-  # })
   
 })
   
