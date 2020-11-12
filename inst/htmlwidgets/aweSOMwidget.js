82b07857511ae1e2af53bdb195ae3a3be647240d
Jan Sodoge <janso162@student.liu.se>
2020-11-12 17:45:57 +0100

    Update aweSOMwidget.js

--- inst/htmlwidgets/aweSOMwidget.js
+++ inst/htmlwidgets/aweSOMwidget.js
@@ -671,7 +671,7 @@ console.log("Enter aweSOMwidget\n");
 
     //remove the old graph
     document.getElementById("theWidget").innerHTML = "";
-    document.getElementById("theWidget").style.marginBottom = "50%";
+    //document.getElementById("theWidget").style.marginBottom = "50%";
     // remove old plot messages
     document.getElementById("cell-info").innerHTML = "Hover over the plot for information.";
     document.getElementById("plot-message").innerHTML = "-";
