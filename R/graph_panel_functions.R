de3e808c6d3be5b93f80366cbb2af6e8dc727c6c
Jan Sodoge <janso162@student.liu.se>
2020-11-13 15:13:11 +0100

    Update graph_panel_functions.R

--- R/graph_panel_functions.R
+++ R/graph_panel_functions.R
@@ -811,6 +811,10 @@ aweSOMplot <- function(ok.som, ok.sc, ok.data, ok.trainrows,
                                                                    style = "padding-bottom:0px")))
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "cell-info")))
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "plot-message")))
+  res <- htmlwidgets::prependContent(res, htmltools::tag("p", list(style = "padding-bottom:500px")))
+  
+  
+  
   res <- htmlwidgets::appendContent(res, htmltools::tag("p", list(id= "plot-names")))
   
  
