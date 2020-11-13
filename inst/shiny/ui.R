a9dbe1476778a504a9e9e030a3c9534320d10149
Jan Sodoge <janso162@student.liu.se>
2020-11-13 14:00:16 +0100

    Update ui.R

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -399,6 +399,7 @@ shinyUI(fluidPage(
                                               HTML('<h4 id="plot-message">-</h4>'),
                                               # HTML('<div id="thePlot" class="shiny-Plot"><svg /></div>'), #JS plots placed here?!
                                               aweSOM:::aweSOMoutput("theWidget"),
+                                              htmltools::tag("p", list(style = paste0("padding-bottom:",5000 ,"px"))),
                                               HTML('<br />'), 
                                               #aweSOM:::aweSOMoutput("plot-names"),
                                               
