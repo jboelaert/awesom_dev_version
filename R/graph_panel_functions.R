b387d43d7a4d2ffc57a045ce98b24e40d0ca1381
Jan Sodoge <janso162@student.liu.se>
2020-11-16 23:06:11 +0100

    adapted layout

--- R/graph_panel_functions.R
+++ R/graph_panel_functions.R
@@ -811,12 +811,12 @@ aweSOMplot <- function(ok.som, ok.sc, ok.data, ok.trainrows,
                                                                    style = "padding-bottom:0px")))
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "cell-info")))
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "plot-message")))
-  res <- htmlwidgets::prependContent(res, htmltools::tag("p", list(id= "plot_names_margin",
-                                                                   style = paste0("padding-bottom:",60 ,"px"))))
+  
      
   
   
-  res <- htmlwidgets::appendContent(res, htmltools::tag("p", list(id= "plot-names")))
+  res <- htmlwidgets::appendContent(res, htmltools::tag("p", list(id= "plot-names",
+                                                                  style = paste0("padding-bottom:",60 ,"%"))))
   
  
 
