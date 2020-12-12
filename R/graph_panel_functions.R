2e54ab66f4f3d308f71a597124ff03c0af8273b8
Jan Sodoge <janso162@student.liu.se>
2020-12-12 15:54:24 +0100

    accient

--- R/graph_panel_functions.R
+++ R/graph_panel_functions.R
@@ -897,9 +897,9 @@ aweSOMplot <- function(ok.som, ok.sc, ok.data, omitRows= NULL,
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "cell-info")))
   res <- htmlwidgets::prependContent(res, htmltools::tag("h4", list(id= "plot-message")))
   
-  
-  #res <- htmlwidgets::appendContent(res, 
-  #                                  (htmltools::tag("svg", list(id= "awesom_legend_svg"))))
+  res <- htmlwidgets::appendContent(res, htmltools::tag("p", list(id= "plot-names")))
+  res <- htmlwidgets::appendContent(res, 
+                                    (htmltools::tag("svg", list(id= "awesom_legend_svg"))))
   
   
 
