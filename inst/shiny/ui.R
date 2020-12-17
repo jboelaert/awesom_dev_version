2bc4d41a084759278a7fe3eb65caae8987f1d094
Jan Sodoge <janso162@student.liu.se>
2020-12-17 13:24:50 +0100

    Update ui.R

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -4,6 +4,7 @@
 ## Organisation de la page
 shinyUI(fluidPage(
   tags$head(HTML("<title>aweSOM</title>")),
+  #to modify style of help messages (increase font etc. tbd after lunch)
   tags$style(
     HTML(".shiny-notification {
              position:fixed;
