647e64ddb0d50eeb3957e6a79de32fe5a051ca15
julbo <jboelaert@univ-lille.fr>
2021-01-19 16:18:41 +0200

    Added aweSOM-package man page

---
+++ R/aweSOM-package.R
@@ -0,0 +1,14 @@
+#' @title Interactive Self-Organizing Maps
+#' @name aweSOM-package
+#' @docType package
+#'
+#' @description Interactive plots and interface for Kohonen self-organizing
+#'   maps.
+#'
+#' @references Kohonen T. (2001) \emph{Self-Organizing Maps}, 3rd edition,
+#'   Springer Press, Berlin.
+#'
+#' @seealso \code{\link{aweSOM}}, \code{\link{aweSOMplot}},
+#'   \code{\link{somInit}}, \code{\link{somQuality}}.
+
+NULL
